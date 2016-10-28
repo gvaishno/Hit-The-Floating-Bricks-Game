fd6fb9091fc84f87540b43b1694f367838ef448d
Vaishno Chaitanya <vaishnochaitanya7@gmail.com>
2016-10-28 10:58:34 +0530

    Add files via upload

---
+++ Assets/Racket.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Racket : MonoBehaviour {
+	
+	public float paddleSpeed = 1f;
+	
+	private Vector2 playerPos = new Vector2 (0, -111f);
+	
+	void Update () 
+	{
+		float xPos = transform.position.x + (Input.GetAxis("Horizontal") * paddleSpeed);
+		playerPos = new Vector3 (Mathf.Clamp (xPos, -81f, 87f), -111f);
+		transform.position = playerPos;
+		
+	}
+}
